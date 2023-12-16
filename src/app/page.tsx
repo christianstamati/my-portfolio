7c020fc9827ff7c6cfc02245ed6302b11cdfe37b
Christian Stamati <christian.stamati@gmail.com>
2023-12-16 19:05:05 +0100

    grid layout tests

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,19 @@
 export default function Home() {
     return (
-        <main className="h-screen overflow-auto">
-            <nav className={"flex justify-center bg-red-200 p-14"}>NAVIGATION</nav>
-            <div className="grid h-full grid-cols-4 grid-rows-3 gap-6 px-16">
-                <div className="col-span-2 row-span-1 flex items-center justify-center rounded-md bg-blue-600">
+        <main className="flex h-screen justify-center overflow-auto">
+            {/*The grid container*/}
+            <div className="grid h-fit grid-cols-4 gap-6 bg-red-500">
+                <div className="col-span-2 flex h-[280px] items-center justify-center rounded-xl bg-blue-600">
                     About Card
                 </div>
-                <div className="col-span-1 row-span-1 flex items-center justify-center rounded-md bg-blue-600">
-                    Download resume
+                <div className="flex h-[280px] w-[280px] items-center justify-center rounded-md bg-blue-600">
+                    About Card
                 </div>
-                <div className="col-span-1 row-span-2 flex items-center justify-center rounded-md bg-blue-600">
-                    Project
+                <div className="flex h-[280px] w-[280px] items-center justify-center rounded-md bg-blue-600">
+                    About Card
                 </div>
-                <div className="col-span-1 row-span-1 flex items-center justify-center rounded-md bg-blue-600">
-                    Youtube
-                </div>
-                <div className="col-span-1 row-span-1 flex items-center justify-center rounded-md bg-blue-600">Map</div>
-                <div className="col-span-1 row-span-2 flex items-center justify-center rounded-md bg-blue-600">
-                    Project
+                <div className="flex h-[280px] w-[280px] items-center justify-center rounded-md bg-blue-600">
+                    About Card
                 </div>
             </div>
         </main>
