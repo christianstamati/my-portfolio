75f1b6a9a223df7d1b399dd91ff064f594efb200
Christian Stamati <christian.stamati@gmail.com>
2023-12-21 20:25:12 +0100

    Update packages, remove react grid layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,10 @@
-"use client";
-import GridLayout from "react-grid-layout";
-
 export default function Home() {
-    const layout = [
-        { i: "a", x: 0, y: 0, w: 1, h: 2, static: true },
-        { i: "b", x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4 },
-        { i: "c", x: 4, y: 0, w: 1, h: 2 },
-    ];
     return (
-        <GridLayout className="layout" layout={layout} cols={12} rowHeight={30} width={1200}>
-            <div className="bg-gray-400" key="a">
-                a
-            </div>
-            <div className="bg-gray-400" key="b">
-                b
-            </div>
-            <div className="bg-gray-400" key="c">
-                c
-            </div>
-        </GridLayout>
+        <div className="flex h-screen justify-center">
+            <div
+                className={
+                    "w-full max-w-[390px] bg-red-500 sm:max-w-[640px] md:max-w-[768px] lg:max-w-[1024px] xl:max-w-[1280px]"
+                }></div>
+        </div>
     );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,24 @@ const config: Config = {
         "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
         "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     ],
-    theme: {},
+    theme: {
+        screens: {
+            sm: "640px",
+            // => @media (min-width: 640px) { ... }
+
+            md: "768px",
+            // => @media (min-width: 768px) { ... }
+
+            lg: "1024px",
+            // => @media (min-width: 1024px) { ... }
+
+            xl: "1280px",
+            // => @media (min-width: 1280px) { ... }
+
+            "2xl": "1536px",
+            // => @media (min-width: 1536px) { ... }
+        },
+    },
     plugins: [],
 };
 export default config;
