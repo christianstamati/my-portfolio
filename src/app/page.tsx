dd8c4dad2f78a233e6307d1c8ab0eafd7ef01749
Christian Stamati <christian.stamati@gmail.com>
2023-12-29 12:49:03 +0100

    removed react layout grid dev dep; updated packages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
         <div className="flex h-screen justify-center">
             <div
                 className={
-                    "w-full max-w-[390px] bg-red-500 sm:max-w-[640px] md:max-w-[768px] lg:max-w-[1024px] xl:max-w-[1280px]"
+                    "w-full max-w-[390px] bg-gray-300 sm:max-w-[640px] md:max-w-[768px] lg:max-w-[1024px] xl:max-w-[1280px]"
                 }></div>
         </div>
     );
