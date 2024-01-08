a3a23389bb9b4082a832f829614d3b04757bf5c1
Christian Stamati <christian.stamati@gmail.com>
2024-01-08 20:31:17 +0100

    added priority to image

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -11,7 +11,7 @@ type CardProps = {
 const Card = (props: CardProps) => {
     return (
         <div className="relative aspect-square overflow-hidden rounded-xl p-8 shadow-md">
-            <Image className="-z-10 object-cover" src={props.image} alt={"portrait"} fill />
+            <Image className="-z-10 object-cover" src={props.image} alt={"portrait"} fill priority />
             {props.badge ? <p>{props.badge}</p> : null}
             <h3 className="text-4xl font-medium">{props.title}</h3>
             <p className="mt-4 w-fit rounded-full border-2 border-gray-200 px-4 py-1 text-xl">{props.category}</p>
